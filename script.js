e4b16e1956ee08461ccf160911b7715783b21305
PrakatNeupane <60876051+PrakatNeupane@users.noreply.github.com>
2022-02-25 08:20:36 +1100

    calculator with functional programming

---
+++ script.js
@@ -0,0 +1,148 @@
+const display1El = document.querySelector('.display-1')
+const display2El = document.querySelector('.display-2')
+const tempResultEl = document.querySelector('.temp-result')
+const numbersEl = document.querySelectorAll('.number');
+const operationEl = document.querySelectorAll('.operation');
+const equalEl = document.querySelector('.equal');
+const clearEl = document.querySelector('.all-clear');
+const clearLastEl = document.querySelector('.last-entity-clear');
+
+let dis1Num = '';
+let dis2Num = ''
+let result = null;
+let lastOperation = '';
+let haveDot = false;
+
+numbersEl.forEach(number => {
+    number.addEventListener('click', e => {
+        // we wanna have one dot clicked even clicked multiple times
+        if (e.target.innerText === '.' && !haveDot) {
+            haveDot = true;
+        } else if (e.target.innerText === '.' && haveDot) {
+            return;
+        }
+        dis2Num += e.target.innerText; // this is string
+        display2El.innerText = dis2Num;
+
+    })
+})
+
+operationEl.forEach(operation => {
+    operation.addEventListener('click', (e) => {
+        // if the user has not clicked any number, we should prevent operation performance
+        if (!dis2Num) return;
+        haveDot = false;
+        const operationName = e.target.innerText;
+        if (dis1Num && dis2Num && lastOperation) {
+            mathOperation();
+        } else {
+            result = parseFloat(dis2Num) // converting string to number
+        }
+        clearVar(operationName);
+        lastOperation = operationName;
+        console.log(result);
+    })
+})
+
+const clearVar = (name = '') => {
+    dis1Num += dis2Num + ' ' + name + ' '
+    display1El.innerText = dis1Num;
+    display2El.innerText = ''
+    dis2Num = '';
+    tempResultEl.innerText = result;
+}
+
+const mathOperation = () => {
+    switch (lastOperation) {
+        case ('X'):
+            result = parseFloat(result) * parseFloat(dis2Num);
+            break;
+        case ('+'):
+            result = parseFloat(result) + parseFloat(dis2Num);
+            break;
+        case ('-'):
+            result = parseFloat(result) - parseFloat(dis2Num);
+            break;
+        case ('/'):
+            result = parseFloat(result) / parseFloat(dis2Num);
+            break;
+        case ('%'):
+            result = parseFloat(result) % parseFloat(dis2Num);
+            break;
+
+    }
+}
+
+equalEl.addEventListener('click', e => {
+    if (!dis1Num || !dis2Num) return; // if there are no numbers, do not perform anything
+    haveDot = false;
+    mathOperation();
+    clearVar();
+    display2El.innerText = result;
+    tempResultEl.innerText = '';
+    dis2Num = result;
+    dis1Num = '';
+})
+
+clearEl.addEventListener('click', e => {
+    display1El.innerText = '0';
+    display2El.innerText = '0';
+    dis1Num = '';
+    dis2Num = '';
+    result = '';
+    tempResultEl.innerText = '0';
+})
+
+clearLastEl.addEventListener('click', e => {
+    display2El.innerText = '';
+    dis2Num = ''
+})
+
+window.addEventListener('keydown', e => {
+    if (
+        e.key === '0' ||
+        e.key === '1' ||
+        e.key === '2' ||
+        e.key === '3' ||
+        e.key === '4' ||
+        e.key === '5' ||
+        e.key === '6' ||
+        e.key === '7' ||
+        e.key === '8' ||
+        e.key === '9' ||
+        e.key === '.'
+    ) {
+        clickButtonEl(e.key);
+    } else if (
+        e.key === '-' ||
+        e.key === '/' ||
+        e.key === '+' ||
+        e.key === '%'
+    ) {
+        clickOperation(e.key);
+    } else if (e.key === '*') {
+        clickOperation('X')
+    } else if (e.key == 'Enter' || e.key === '=') {
+        clickEqual()
+    }
+});
+
+const clickButtonEl = key => {
+    numbersEl.forEach(button => {
+        if (button.innerText === key) {
+            button.click();
+        }
+    })
+}
+
+const clickOperation = key => {
+    operationEl.forEach(button => {
+        if (button.innerText === key) {
+            button.click();
+        }
+    })
+}
+
+const clickEqual = () => {
+    equalEl.click(); ``
+}
